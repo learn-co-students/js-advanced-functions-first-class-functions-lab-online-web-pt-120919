63d097ae1c064384c39f21870ef9b1ba4857adf8
Jeffrey Martinez <martinezjf2@gmail.com>
2022-01-27 11:52:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 // Code your solution in this file!
 
-const returnFirstTwoDrivers = function(array) {
-    return array.slice(0, 2);
-};
-//Need help explaining this method underneath about using the Math.max
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
 
-const returnLastTwoDrivers = function(array) {
-    return array.slice(Math.max(array.length - 2));
+const returnFirstTwoDrivers = function() {
+  newArray = drivers[0], drivers[1];
 };
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-function createFareMultiplier(fare) {
-    return function(num) {
-        return num * fare;
-    };
+const returnLastTwoDrivers = function() {
+   ;
 };
 
-const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
 
-//Need help explaining this method underneath
-function selectDifferentDrivers(array, f) {
-    return f(array);
-};
