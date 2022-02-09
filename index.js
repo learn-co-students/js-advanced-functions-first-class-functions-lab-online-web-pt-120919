61a605c5552c02054f41fc170b8bef265aa8d010
Jeffrey Martinez <martinezjf2@gmail.com>
2022-02-09 11:20:20 -0500

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
 
 const returnFirstTwoDrivers = function() {
- [drivers[0], drivers[1]];
+//  [drivers[0], drivers[1]];
+return drivers.slice(0, 2)
   
 };
 
