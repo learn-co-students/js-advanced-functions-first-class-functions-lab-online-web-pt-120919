76e2a483483cdc798945dee045fc25d5a9eb8ef4
Jeffrey Martinez <martinezjf2@gmail.com>
2022-02-22 14:14:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Code your solution in this file!
 
-// drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+let drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
 
-const returnFirstTwoDrivers = function(drivers) {
-//  [drivers[0], drivers[1]];
+const returnFirstTwoDrivers = function() {
+newArray = [drivers[0], drivers[1]];
 
-return drivers.slice(0, 2)
-  
 };
+
+
 
 const returnLastTwoDrivers = function() {
   const newArray = [drivers[2], drivers[3]];
-  return newArray
 };
 
 
